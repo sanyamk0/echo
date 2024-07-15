1d6d7f2bca79658a537e5828dba5f016c5e3dba5
sanyamk0 <sanyamkumar002@gmail.com>
2024-07-15 18:10:31 +0530

    added protection of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Authenticate from "./pages/Authenticate";
 import Activate from "./pages/Activate";
+import { useSelector } from "react-redux";
+
+const GuestRoute = ({ children }) => {
+  const { isAuth } = useSelector((state) => state.auth);
+  return isAuth ? <Navigate to="/rooms" /> : children;
+};
+
+const SemiProtectedRoute = ({ children }) => {
+  const { user, isAuth } = useSelector((state) => state.auth);
+  return !isAuth ? (
+    <Navigate to="/" />
+  ) : isAuth && user && !user.activated ? (
+    children
+  ) : (
+    <Navigate to="/rooms" />
+  );
+};
+
+const ProtectedRoute = ({ children }) => {
+  const { user, isAuth } = useSelector((state) => state.auth);
+  return !isAuth ? (
+    <Navigate to="/" />
+  ) : isAuth && user && !user.activated ? (
+    <Navigate to="/activate" />
+  ) : (
+    children
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <GuestRoute>
+        <Home />
+      </GuestRoute>
+    ),
   },
   {
     path: "/authenticate",
-    element: <Authenticate />,
+    element: (
+      <GuestRoute>
+        <Authenticate />
+      </GuestRoute>
+    ),
   },
   {
     path: "/activate",
-    element: <Activate />,
+    element: (
+      <SemiProtectedRoute>
+        <Activate />
+      </SemiProtectedRoute>
+    ),
   },
 ]);
 
